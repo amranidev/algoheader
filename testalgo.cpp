04fb56033c8bc9069d689876d0314bdcfdcfe00c
amranidev <amranidev@gmail.com>
2015-06-29 22:47:36 +0200

    Running an example algoheader

---
+++ testalgo.cpp
@@ -0,0 +1,25 @@
+#include "algoheader.h"
+
+
+
+debut 
+ entier age;
+ 
+  ecrire("donner votre age :");
+  
+  lire("%i", de age);
+  
+  si(age > 18) fair 
+  
+ 
+  ecrire("majeur");
+    finsi 
+  sinonsi( age < 18) fair
+   
+  ecrire("mineur");
+   finsi
+  sinon fair
+  
+  ecrire("MDR");
+finsi
+fin
